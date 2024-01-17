f529faca6433ad7f62364958f2400faa4ea88a53
jadraguirre <joel@steeleconsult.com>
2024-01-17 15:31:48 -0700

    Implemented move model

--- src/app/models/move.ts
+++ src/app/models/move.ts
@@ -1,3 +1,9 @@
 export interface Move {
-    
+    id: number,
+    moveName: string,
+    power: number,
+    powerPoints: number,
+    accuracy: number,
+    flavorText: string,
+    effect: string
 }
