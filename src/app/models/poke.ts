5daa2611a40e2ba23b7915f04ff0053b9237b51c
jadraguirre <joel@steeleconsult.com>
2024-01-17 15:43:24 -0700

    Added ability model and mostly implemented pokemon model

--- src/app/models/poke.ts
+++ src/app/models/poke.ts
@@ -1,3 +1,23 @@
+import { Ability } from "./ability";
+import { Move } from "./move";
+import { Type } from "./type";
+
 export interface Pokemon {
-    
+    id: number,
+    pokemonName: string,
+    baseExperience: number,
+    weight: number,
+    height: number,
+    baseAttack: number,
+    baseDefense: number,
+    flavorText: string,
+    genus: string,
+    evolutionLevel: number,
+    evolvesInto: string,
+    evolutionTrigger: string,
+    abilities: Ability[]
+    moves: Move[],
+    types: Type[],
+    generations: string[],
+    versions: string[]
 }
