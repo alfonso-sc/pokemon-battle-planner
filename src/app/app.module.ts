9a401fea0423ba7d5053432b492d84ef1a72c892
alfonso <alfonso@steeleconsult.com>
2024-01-17 15:32:33 -0700

    first starts at lobby and trainer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LobbyComponent } from './components/lobby/lobby.component';
-import { TrainerComponent } from './components/trainer/trainer.component';
-import { ResultsComponent } from './components/results/results.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { ResultsComponent } from './components/results/results.component';
+import { TrainerComponent } from './components/trainer/trainer.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,11 @@ import { PokemonComponent } from './components/pokemon/pokemon.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
