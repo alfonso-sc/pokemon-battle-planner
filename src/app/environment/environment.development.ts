4572b7bda1dea93d8a98e19c458f858c0cb9dc9b
jadraguirre <joel@steeleconsult.com>
2024-01-17 14:08:41 -0700

    Created models, environment with API link

---
+++ src/app/environment/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    apiUrl: "https://steele-pokedex.azurewebsites.net/api"
+};
--- src/app/services/poke-service.service.ts
+++ src/app/services/poke-service.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PokemonList } from '../models/poke-list';
+import { Pokemon } from '../models/poke';
+import { environment } from '../environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeServiceService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getMany(pageSize?: number) {
+    let params: HttpParams = new HttpParams();
+    if (pageSize) params = params.append("pageSize", pageSize)
+    return this.http.get<PokemonList>(`${environment.apiUrl}/Pokemon`,{params});
+  }
+
+  getMon(identifier: string){
+    return this.http.get<Pokemon>(`${environment.apiUrl}/Pokemon/${identifier}`);
+  }
 }
