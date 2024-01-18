832a8f55bdf3a956e7787a9b0d7361e67d02dd48
alfonso <alfonso@steeleconsult.com>
2024-01-18 08:44:55 -0700

    finish adding html elements with hardcoded data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -27,6 +29,8 @@ import { TrainerComponent } from './components/trainer/trainer.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
