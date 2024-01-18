30fef8547c42de6644251f845958e84aaf0b63f2
alfonso <alfonso@steeleconsult.com>
2024-01-18 08:20:25 -0700

    continue building out trainer

--- src/app/components/trainer/trainer.component.ts
+++ src/app/components/trainer/trainer.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { PokemonList } from '../../models/poke-list';
+import { PokeServiceService } from '../../services/poke-service.service';
 
 @Component({
   selector: 'app-trainer',
@@ -7,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class TrainerComponent {
 
+  pokemonResponse$!: Observable<PokemonList>;
+
+  constructor(private pokeService: PokeServiceService) { }
+
+  getPokemon() {
+    this.pokemonResponse$ = this.pokeService.getMany()
+  }
+
+  // use to console.log from html for debugging, etc.
+  html_log(val: any): void {
+    console.log(val);
+  }
 }
+
