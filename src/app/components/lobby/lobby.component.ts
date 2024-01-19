b73a44f9255a3458805b4bb700ba87acd96edf84
alfonso <alfonso@steeleconsult.com>
2024-01-19 10:40:54 -0700

    bring pokemon data to lobby in prep for battle calculations

--- src/app/components/lobby/lobby.component.ts
+++ src/app/components/lobby/lobby.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { PokemonBattle } from '../../models/poke-battle';
+import { TrainerComponent } from '../trainer/trainer.component';
 
 @Component({
   selector: 'app-lobby',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './lobby.component.scss'
 })
 export class LobbyComponent {
+  @ViewChild('heroComponent') hero: TrainerComponent;
+  @ViewChild('opponentComponent') opponent: TrainerComponent;
 
+  heroBattlePokemon!: PokemonBattle[];
+  opponentBattlePokemon!: PokemonBattle[];
+
+  constructor() {
+    this.hero = {} as TrainerComponent;
+    this.opponent = {} as TrainerComponent;
+  }
+
+  receiveDataFromHero(battlePokemon: PokemonBattle[]): void {
+    this.heroBattlePokemon = battlePokemon;
+  }
+
+  receiveDataFromOpponent(battlePokemon: PokemonBattle[]): void {
+    this.opponentBattlePokemon = battlePokemon;
+  }
+
+  onClickFindMatches(): void {
+    console.log("heroBattlePokemon", this.heroBattlePokemon)
+    console.log("opponentBattlePokemon", this.opponentBattlePokemon)
+  }
 }
